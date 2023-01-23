39af9bebffc6a92f0e22d9ac05712e2cbb13e6a8
Vitor Rogatto Lopes <vitorrogatto9@hotmail.com>
2023-01-23 17:00:39 -0300

    j

---
+++ aula20/js/index.js
@@ -0,0 +1,23 @@
+function meuEscopo(){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = []
+
+    function recebeEventoForm(evento){
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura= form.querySelector('.altura')
+
+        evento.preventDefault();
+    
+    function listaCompleta(nome, sobrenome, peso, altura){
+        lista1 = listaCompleta
+    }
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+}
+meuEscopo();
