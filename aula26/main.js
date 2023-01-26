7ca2fca2fe44ac17d69325c3c87a3a06414c526f
Vitor Rogatto Lopes <vitorrogatto9@hotmail.com>
2023-01-26 12:30:25 -0300

    f

--- aula26/main.js
+++ aula26/main.js
@@ -1,6 +1,37 @@
-const formulario = document.querySelector('#form')
+const form = document.querySelector('#formulario')
 
-formulario.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e){
     e.preventDefault()
-    console.log('evento previnido')
+    const inputPeso = form.querySelector('#peso')
+    const inputAltura = form.querySelector('#altura')
+
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if(!peso){
+        setResultado('Peso invalido', false)
+        return
+    }
+
+    if(!altura){
+        setResultado('Altura invalida', false)
+        return
+    }
+
+    console.log('cheguei aqui')
+    
 })
+
+function criaP(className){
+    const p = document.createElement('p');
+    return p
+}
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML ='';
+
+    const p = criaP()
+    p.innerHTML = msg
+    resultado.appendChild(p)
+}
