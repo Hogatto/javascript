031a216622e64333f4b9f87e98dd4321d1ad334b
Vitor Rogatto Lopes <vitorrogatto9@hotmail.com>
2023-01-27 15:14:35 -0300

    f

--- aula26/main.js
+++ aula26/main.js
@@ -18,9 +18,28 @@ form.addEventListener('submit', function (e){
         return
     }
 
-    console.log('cheguei aqui')
-    
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
+
+    const msg = `Seu IMC é ${imc}(${nivelImc})`
+    setResultado(msg, true)
 })
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'] 
+
+    if (imc >= 39.9) return nivel[5]
+    if (imc >= 34.9) return nivel[4]
+    if (imc >= 29.9)return nivel[3]
+    if (imc >= 24.9) return nivel[2]
+    if (imc >= 18.5)return nivel[1]
+    if (imc < 18.5) return nivel [0] 
+}
+
+function getImc(peso ,altura){
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
 
 function criaP(className){
     const p = document.createElement('p');
@@ -32,6 +51,13 @@ function setResultado(msg, isValid) {
     resultado.innerHTML ='';
 
     const p = criaP()
+
+    if(isValid) {
+        p.classList.add('paragrafo-resultado')
+    }else{
+        p.classList.add('bad')
+    }
+
     p.innerHTML = msg
     resultado.appendChild(p)
 }
---
+++ aula27/index.js
@@ -0,0 +1,2 @@
+const data = new Date('2023-5-7 20:30:00')
+console.log(data.toString())
