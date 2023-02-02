54faab5be621ce248728dca3609126441ba6c4b2
Vitor Rogatto Lopes <vitorrogatto9@gmail.com>
2023-02-02 15:49:19 -0300

    f

--- aula35/js.js
+++ aula35/js.js
@@ -2,9 +2,9 @@ const paragrafos = document.querySelector('.paragrafos')
 const ps = paragrafos.querySelectorAll('p')
 
 const estilosBody = getComputedStyle(document.body)
-const backgroundColorBody = estilosBody.backgroundColor
+const backgroundColorBody = estilosBodbackgroundColory.
 console.log(backgroundColorBody)
 
 for (let p of ps){
-    p.style.backgroundColor = backgroundColorBody
+    p.style.ackgrobundColor = backgroundColorBody
 }
