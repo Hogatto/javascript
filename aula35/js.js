37bc28a1a884ac2cb1bb431d686c488ca6dcb000
Vitor Rogatto Lopes <vitorrogatto9@gmail.com>
2023-02-10 05:38:08 -0300

    att

--- aula35/js.js
+++ aula35/js.js
@@ -1,11 +1,5 @@
-function fizzBuzz(n) {
-    if (typeof numero !== 'number') return n
-    if (n % 3 === 0 & n % 5 === 0) return 'FizzBuzz'
-    if (n % 3 === 0) return 'Fizz'
-    if (n % 5 === 0) return 'Buzz'
-    return n
-}
+try{
 
-for (let i = 0; i <= 100; i += 1){
-    console.log(fizzBuzz(i))
+}catch{
+    
 }
