cc6023a9d18d16277d40093e53d30f9a0225556a
Vitor Rogatto Lopes <vitorrogatto9@gmail.com>
2023-02-10 07:18:16 -0300

    att

--- aula35/js.js
+++ aula35/js.js
@@ -1,7 +1,21 @@
-try{
-    console.log(ae)
-}catch{
-    console.log('trantando a')
-}finally {
-    const ae = 'ola'
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {hour12: false})
 }
+
+iniciar.addEventListener('click', function(event){
+    
+})
+
+pausar.addEventListener('click', function(event){
+    alert('cliquei no pausar')
+})
+
+zerar.addEventListener('click', function(event){
+    alert('cliquei no zerar')
+})
