f94cfd8fdbb760278fc2b628e8e8e5781fe9c236
Vitor Rogatto Lopes <vitorrogatto9@gmail.com>
2023-03-01 08:45:17 -0300

    Update index.js

--- aula15.js/js/index.js
+++ aula15.js/js/index.js
@@ -1,11 +1,8 @@
-const num = Number(prompt('Digite seu numero: '))
-const numeroTitulo = document.getElementById('numero-titulo')
-const texto = document.getElementById('texto')
+function recursiva(max) {
+    if (max >= 10) return
+    max += 1;
+    console.log(max)
+    recursiva(max)
+}
 
-numeroTitulo.innerHTML = num
-texto.innerHTML += `<p>Raiz Quadrada: ${num ** 0.5}</p>`
-texto.innerHTML += `<p>Seu número é inteiro: ${Number.isInteger(num)}</p>`
-texto.innerHTML += `<p>é NaN: ${Number.isNaN(num)}</p>`
-texto.innerHTML += `<p>Arredondado pra baixo: ${Math.floor(num)}</p>`
-texto.innerHTML += `<p>Arredondado pra cima: ${Math.ceil(num)}</p>`
-texto.innerHTML += `<p>Com duas casas decimais: ${num.toFixed(2)}</p>`
+recursiva(0)
