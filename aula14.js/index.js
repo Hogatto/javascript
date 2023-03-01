d4fefb2a1c8c0c530e387b2b0d9e708f1ea3428b
Vitor Rogatto Lopes <vitorrogatto9@gmail.com>
2023-03-01 16:39:12 -0300

    Update index.js

---
+++ aula14.js/index.js
@@ -0,0 +1,22 @@
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosFiltrados = numeros.filter(valor => valor > 10)
+const numerosEmDobro = numeros.map(function(valor, indice, array){
+    return valor
+})
+
+
+
+
+
+
+
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47}
+]
